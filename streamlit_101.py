06692d2b0430b4c2a7a17c306b72c11d4fdf7335
SAMET GİRGİN <44918645+sametgirgin@users.noreply.github.com>
2024-08-28 09:26:43 +0000

    first commit

---
+++ streamlit_101.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title("Can you see my Portfolio!!!")
+
+# Import matplotlib.pyplot with alias plt
+import matplotlib.pyplot as plt
+import pandas as pd
+@st.cache_data
+def load_data():
+    return pd.read_csv('https://raw.githubusercontent.com/kzavrazhnyi/pythonthefirst/master/avocados_2016.csv')
+
+avocados_2016 = load_data()
+
+st.header("1-Inspect the dataset")
+st.write("'st.data_editor' allow us to display and edit the dataset")
+
+st.data_editor(avocados_2016)
+
+st.bar_chart(avocados_2016.isna().sum())
